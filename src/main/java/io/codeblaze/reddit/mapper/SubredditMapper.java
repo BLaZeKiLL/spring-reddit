7aa23f9ab76b82bf9a4b3e7a76516920f2e62586
Devashish Lal <laldevashish@gmail.com>
2020-06-24 21:59:20 +0530

    SubredditMapper created

--- src/main/java/io/codeblaze/reddit/mapper/SubredditMapper.java
+++ src/main/java/io/codeblaze/reddit/mapper/SubredditMapper.java
@@ -2,6 +2,7 @@ package io.codeblaze.reddit.mapper;
 
 import io.codeblaze.reddit.dto.SubredditDto;
 import io.codeblaze.reddit.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,5 +11,9 @@ public interface SubredditMapper {
 
     @Mapping(target = "numberOfPosts", expression = "java(subreddit.getPosts().size())")
     SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
 
 }
